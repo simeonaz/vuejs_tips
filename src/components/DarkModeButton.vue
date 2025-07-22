9759cabb294050cb1e422e4db21efc5210579a4d
Siméon A. <simeonazogbonon@gmail.com>
2025-07-22 22:35:50 +0100

    minor changes

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -11,8 +11,7 @@ const toggleDark = useToggle(isDark);
     type="button"
     class="cursor-pointer flex-shrink-0 rounded-md gap-x-1.5 p-1.5 inline-flex items-center"
   >
-    <!-- Feel free to adapt these icons  -->
-    <span v-if="isDark">☀️</span>
-    <span v-else>🌙</span>
+    <!-- Feel free to customize these icons  -->
+    {{ isDark ? "☀️" : "🌙" }}
   </button>
 </template>
